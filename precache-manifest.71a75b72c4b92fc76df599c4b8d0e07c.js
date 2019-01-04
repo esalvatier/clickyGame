2d4f8c1a93516a20828b239550d29ee7eb0368e9
Erik Salvatier <m.erik.salvatier@gmail.com>
2019-01-04 12:47:03 -0800

    Updates

---
+++ precache-manifest.71a75b72c4b92fc76df599c4b8d0e07c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5e1124212e913e930db",
+    "url": "/clickyGame/static/js/runtime~main.f5e11242.js"
+  },
+  {
+    "revision": "1892534483e256971408",
+    "url": "/clickyGame/static/js/main.18925344.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clickyGame/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1892534483e256971408",
+    "url": "/clickyGame/static/css/main.0f71d1cf.chunk.css"
+  },
+  {
+    "revision": "d43ab5b377cc512db77ab1e43a7fc0ea",
+    "url": "/clickyGame/index.html"
+  }
+];
